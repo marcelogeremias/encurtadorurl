74890f3acf02766380ded3821711ebd30cdd00cb
Marcelo Henrique <marcelo.henrique@locaweb.com.br>
2020-11-29 17:38:30 -0300

    inclusao de variaveis de ambiente no projeto

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header';
 import { Container, InputGroup, FormControl, Button, Alert, Spinner } from 'react-bootstrap';
 import { ContentContainer, Form, AdsBlock } from './styles';
 import ShortenerService from '../../services/shortenerService';
+import vars from '../../configs/vars';
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -69,14 +70,14 @@ class HomePage extends React.Component {
                                     <InputGroup className="mb-3">
                                         <FormControl 
                                             autoFocus ={true}
-                                            defaultValue={`http://localhost:3000/${code}`}
+                                            defaultValue={vars.HOST_APP + code}
                                             ref={(input) => this.inputURL = input}
                                         />
                                         <InputGroup.Append>
                                             <Button variant="outline-secondary" onClick={() => this.copyToClipboard()}>Copiar</Button>
                                         </InputGroup.Append>
                                     </InputGroup>
-                                    <p>Para acompanhar as estatísticas, acesse http://localhost:3000/{code}/stats .</p>
+                                    <p>Para acompanhar as estatísticas, acesse {vars.HOST_APP + code}/stats .</p>
                                 </>
                             )
                         )}
